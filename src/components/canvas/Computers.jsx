53058bb0db9fc19c12d3334a5a4276c94749e6f4
Harsimran Bhinder <bhinderrr@gmail.com>
2023-11-16 15:03:56 -0500

    creating about section divs and adding basic info

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -8,10 +8,10 @@ const Computers = ({ isMobile }) => {
 
   return (
     <mesh>
-      <hemisphereLight intensity={0.15} groundColor="black" />
+      <hemisphereLight intensity={2.15} groundColor="black" />
       <pointLight intensity={1} />
       <spotLight
-        position={[-20, 50, 10]}
+        position={[-10, 50, 10]}
         angle={0.12}
         penumbra={1}
         intensity={1}
