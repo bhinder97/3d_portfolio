a617dd91632f61bf3b3a81feee55a2a87ee37b1e
Harsimran Bhinder <bhinderrr@gmail.com>
2023-11-16 14:17:17 -0500

    linting

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -16,11 +16,14 @@ const Computers = () => {
         penumbra={1}
         intensity={1}
         castShadow
-        shadow-mapSize={1024} />
-      <primitive object={computer.scene}
-      scale={0.75} 
-      position={[0, -3.25, -1.5]}
-      rotation={[-0.01, -0.2, -0.1]}/>
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={0.75}
+        position={[0, -3.25, -1.5]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
     </mesh>
   );
 };
@@ -28,22 +31,22 @@ const Computers = () => {
 const ComputersCanvas = () => {
   return (
     <Canvas
-    frameloop="demand"
-    shadows
-    camera={{ position: [20, 3, 5], fov: 25}}
-    gl={{ preserveDrawingBuffer: true }}
+      frameloop="demand"
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
     >
-      <Suspense >
-        <OrbitControls 
-        enableZoom={false} 
-        maxPolarAngle={Math.PI / 2} 
-        minPolarAngle={Math.PI / 2} 
+      <Suspense>
+        <OrbitControls
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
         />
         <Computers />
       </Suspense>
       <Preload all />
     </Canvas>
-  )
-}
+  );
+};
 
 export default ComputersCanvas;
